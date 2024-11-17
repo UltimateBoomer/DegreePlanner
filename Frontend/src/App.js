949a4611ae886e69931c26df2d9652c578318eb0
Gateek <chandakgateek@gmail.com>
2024-11-17 13:57:38 -0500

    initialized router and set up tailwind

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DegreePlanner from './Pages/DegreePlanner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/degree-planner" element={<DegreePlanner />} />
+          {/* 404 Route
+          <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ Frontend/src/Pages/DegreePlanner.jsx
@@ -0,0 +1,9 @@
+const DegreePlanner = () => {
+    return ( 
+        <div>
+            <h1 className="text-blue-500">DEGREE Planner</h1>
+        </div>
+     );
+}
+ 
+export default DegreePlanner;
