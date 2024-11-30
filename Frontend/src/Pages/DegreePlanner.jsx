15454bb2c849a09313aed35a00f4f1f85711dee1
Gateek <chandakgateek@gmail.com>
2024-11-30 17:11:17 -0500

    Rebuilt frontend with Vite + React + Tailwind + Typesript

--- Frontend/src/Pages/DegreePlanner.jsx
+++
@@ -1,9 +0,0 @@
-const DegreePlanner = () => {
-    return ( 
-        <div>
-            <h1 className="text-blue-500">DEGREE Planner</h1>
-        </div>
-     );
-}
- 
-export default DegreePlanner;
